91e9b86f095b6ed3aaef6105a64cbc01a9205952
David Grimes <david@shippable.com>
2015-02-02 17:23:46 -0800

    wwww2

--- src/test/scala/com/shippable/TestHello.scala
+++ src/test/scala/com/shippable/TestHello.scala
@@ -2,10 +2,11 @@ package com.shippable
 
 import org.scalatest.junit.JUnitSuite
 import junit.framework.Assert._
+import org.junit.Test
 
 class TestStuff extends JUnitSuite {
  val calc = new HelloWorldiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
- 
+
  @ Test def oneIsOne {
     assertEquals(1, calc square 1)
   }
